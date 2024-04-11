70d0cbd8f6ac36f7315cc34012f2f9f77c10ddae
yoliveros <28690224+yoliveros@users.noreply.github.com>
2024-04-11 09:09:01 -0400

    feat: telescope find config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,3 +6,8 @@ vim.keymap.set('n', '<leader>ps', function()
 end, {})
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+
+-- Shorcut for nvim config files
+vim.keymap.set("n", "<leader>nc", function()
+    builtin.find_files { cwd = vim.fn.stdpath 'config' }
+end)
--- lua/yoliveros/remap.lua
+++ lua/yoliveros/remap.lua
@@ -13,12 +13,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set("n", "<leader>y", [["+y]])
-vim.keymap.set("v", "<leader>Y", [["+Y]])
-vim.keymap.set("v", "<leader>y", [["+y]])
 
-vim.keymap.set("n", "<C-f>", "<cmd>!tmux neww tmux-sessionizer<CR>", { silent = true })
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format)
 
 -- navigate quickfix
@@ -26,6 +24,7 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
 
 -- search and replace
 vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<Left><Left><Left>]])
