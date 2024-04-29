a1a5f228b710b4be16db164c1b8ab5a45369702a
yoliveros <28690224+yoliveros@users.noreply.github.com>
2024-04-29 10:28:23 -0400

    feat: trying transparent background

--- lua/yoliveros/remap.lua
+++ lua/yoliveros/remap.lua
@@ -14,7 +14,6 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format)
